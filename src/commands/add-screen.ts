67048810fe67505a73d6cbc3b41e5e263292ced5
Ross <ross@slaney.org>
2020-11-23 14:11:15 -0800

    working

--- src/commands/add-screen.ts
+++ src/commands/add-screen.ts
@@ -5,20 +5,21 @@ import { GluegunToolbox } from 'gluegun'
 module.exports = {
   name: 'add-screen',
   run: async (toolbox: GluegunToolbox) => {
-    const {
-      parameters,
-      template: { generate },
-      print: { info },
-    } = toolbox
-
+    const { parameters, print, template} = toolbox
     const name = parameters.first
 
-    await generate({
-      template: 'model.ts.ejs',
-      target: `models/${name}-model.ts`,
-      props: { name },
+    await template.generate({
+      template: `screen.tsx.ejs`,
+      target: `Screens/${name}/${name}.tsx`,
+      props: { name }
     })
 
-    info(`Generated file at models/${name}-model.ts`)
+    await template.generate({
+      template: `codebehind.ts.ejs`,
+      target: `Screens/${name}/${name}.codebehind.ts`,
+      props: { name }
+    })
+
+    print.info('Generated screen and codebehind file. @TODO update app.tsx to connect screen to SessionStore and NavTree')
   },
 }
