55c735a4c05bd4c7a5f29fea58162a3bec8937df
Ross <ross@slaney.org>
2020-11-24 10:27:46 -0800

    fix the service templates

--- src/commands/convertToApp.ts
+++ src/commands/convertToApp.ts
@@ -4,7 +4,7 @@ import { GluegunCommand } from 'gluegun'
 const command: GluegunCommand = {
   name: 'convertToApp',
   run: async toolbox => {
-    const { parameters, print, template, system} = toolbox
+    const { parameters, print, template} = toolbox
     const name = parameters.first
     print.info('Installing the starter app templates')
     await template.generate({
@@ -110,55 +110,55 @@ const command: GluegunCommand = {
 
     // @TODO
     // uncomment once the templates are working correctly -- should probably just be a seperate command
-      const stdioMode = parameters.options.debug ? 'inherit' : 'ignore'
-      var cmd = 'npm install'; 
-      print.info(`command: ${cmd}`)
-      await system.exec(cmd, { stdio: stdioMode })
+    //   const stdioMode = parameters.options.debug ? 'inherit' : 'ignore'
+    //   var cmd = 'npm install'; 
+    //   print.info(`command: ${cmd}`)
+    //   await system.exec(cmd, { stdio: stdioMode })
 
-      cmd = 'cd ios && pod install && cd .. && npx react-native link'
-      print.info(`command: ${cmd}`)
-      await system.exec(cmd, { stdio: stdioMode })
+    //   cmd = 'cd ios && pod install && cd .. && npx react-native link'
+    //   print.info(`command: ${cmd}`)
+    //   await system.exec(cmd, { stdio: stdioMode })
 
-      cmd = 'npm install --save react-navigation react-native-gesture-handler react-native-reanimated react-native-screens';
-      print.info(`command: ${cmd}`)
-      await system.exec(cmd, { stdio: stdioMode })
+    //   cmd = 'npm install --save react-navigation react-native-gesture-handler react-native-reanimated react-native-screens';
+    //   print.info(`command: ${cmd}`)
+    //   await system.exec(cmd, { stdio: stdioMode })
 
-      // new
+    //   // new
 
-      cmd ='cd ios && pod install && cd ..';
-      print.info(`command: ${cmd}`)
-      await system.exec(cmd, { stdio: stdioMode })
+    //   cmd ='cd ios && pod install && cd ..';
+    //   print.info(`command: ${cmd}`)
+    //   await system.exec(cmd, { stdio: stdioMode })
 
-      cmd ='npx react-native unlink react-native-gesture-handler ';
-      print.info(`command: ${cmd}`)
-      await system.exec(cmd, { stdio: stdioMode })
+    //   cmd ='npx react-native unlink react-native-gesture-handler ';
+    //   print.info(`command: ${cmd}`)
+    //   await system.exec(cmd, { stdio: stdioMode })
 
-      cmd ='npm uninstall react-navigation';
-      print.info(`command: ${cmd}`)
-      await system.exec(cmd, { stdio: stdioMode })
+    //   cmd ='npm uninstall react-navigation';
+    //   print.info(`command: ${cmd}`)
+    //   await system.exec(cmd, { stdio: stdioMode })
 
-      cmd ='npm uninstall react-navigation-gesture-handler';
-      print.info(`command: ${cmd}`)
-      await system.exec(cmd, { stdio: stdioMode })
+    //   cmd ='npm uninstall react-navigation-gesture-handler';
+    //   print.info(`command: ${cmd}`)
+    //   await system.exec(cmd, { stdio: stdioMode })
 
-      cmd ='npm install --save react-navigation';
-      print.info(`command: ${cmd}`)
-      await system.exec(cmd, { stdio: stdioMode })
+    //   cmd ='npm install --save react-navigation';
+    //   print.info(`command: ${cmd}`)
+    //   await system.exec(cmd, { stdio: stdioMode })
 
-      cmd ='npm install --save react-native-gesture-handler ';
-      print.info(`command: ${cmd}`)
-      await system.exec(cmd, { stdio: stdioMode })
+    //   cmd ='npm install --save react-native-gesture-handler ';
+    //   print.info(`command: ${cmd}`)
+    //   await system.exec(cmd, { stdio: stdioMode })
       
-      cmd ='npx react-native link react-native-gesture-handler' ;
-      print.info(`command: ${cmd}`)
-      await system.exec(cmd, { stdio: stdioMode })
+    //   cmd ='npx react-native link react-native-gesture-handler' ;
+    //   print.info(`command: ${cmd}`)
+    //   await system.exec(cmd, { stdio: stdioMode })
 
-      cmd ='cd ios && pod install && cd ..';
-      print.info(`command: ${cmd}`)
-      await system.exec(cmd, { stdio: stdioMode })
+    //   cmd ='cd ios && pod install && cd ..';
+    //   print.info(`command: ${cmd}`)
+    //   await system.exec(cmd, { stdio: stdioMode })
 
-    print.success('Successfully installed the starter app template');
-    print.success('Use npx react-native start --reset-cache');
+    // print.success('Successfully installed the starter app template');
+    // print.success('Use npx react-native start --reset-cache');
   },
 }
 
